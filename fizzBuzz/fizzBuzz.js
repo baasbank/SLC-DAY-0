f3a4864a73fbe88a650cc10f1122dd727eea4e5a
Baasbank Akinmuleya <baasb4real@yahoo.com>
2017-04-08 05:53:02 +0100

    Checks divisibility by 5 alone

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -1,5 +1,8 @@
 function fizzBuzz(val)  { 
- if (val % 3 == 0 && val % 5 == 0) {
+    if (val % 3 == 0 && val % 5 == 0) {
         return "fizzBuzz";
       }
+    else if (val % 5 == 0) {
+        return "buzz";
+        }  
 }
