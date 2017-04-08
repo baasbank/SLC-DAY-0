554e6c059014c134fe7dcc316d84e71a82b11da0
Baasbank Akinmuleya <baasb4real@yahoo.com>
2017-04-08 06:10:36 +0100

    Checks that only numbers are supplied as input

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -1,14 +1,19 @@
-function fizzBuzz(num)  { 
-    if (num % 3 == 0 && num % 5 == 0) {
-        return "fizzBuzz";
-      }
-    else if (num % 5 == 0) {
-        return "buzz";
-    } 
-    else if (num % 3 == 0) {
-        return "fizz";
-    } 
-    else {
-        return num ;
+function fizzBuzz(num)  {
+    if (typeof num == 'number') { 
+        if (num % 3 == 0 && num % 5 == 0) {
+            return "fizzBuzz";
         }
+        else if (num % 5 == 0) {
+            return "buzz";
+        } 
+        else if (num % 3 == 0) {
+            return "fizz";
+        } 
+        else {
+            return num ;
+        }
+    }
+    else {
+console.log("Numbers only");  
+    }
 }
