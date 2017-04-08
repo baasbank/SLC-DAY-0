1c2ed901d37bb2673816ebc3770450dbd5f13d3b
Baasbank Akinmuleya <baasb4real@yahoo.com>
2017-04-08 07:57:44 +0100

    Checks if there's a common difference

--- aritGeo/src/aritGeo.js
+++ aritGeo/src/aritGeo.js
@@ -2,4 +2,17 @@ function aritGeo(arr){
     if (arr.length == 0) {
         return 0;
     }
+    else {
+        var diff = arr[1] - arr[0];
+        var ratio = arr[1]/arr[0];
+
+        var arithmetic = true;
+        var geometric = true;
+
+        for (var i = 0; i < arr.length- 1; i++){
+            if (arr[i + 1] - arr[i] !== diff ) {
+                arithmetic = false;
+            }
+        }
+    }
 }
