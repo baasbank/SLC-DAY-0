aeb86ce2fafcacf36d290bd06c28ad8e50a3db60
Baasbank Akinmuleya <baasb4real@yahoo.com>
2017-04-08 08:41:04 +0100

    Checks for and returns all prime numbers in range

--- getPrimes/src/getPrimes.js
+++ getPrimes/src/getPrimes.js
@@ -17,5 +17,35 @@ var getPrimes = function (n)
 		  // return "invalid input" if argument is less that equal to zero
 			return "invalid input";
 		}
-    }
-}
+        else
+		{
+			for(var i = 1; i <= n; i++)
+			{
+					for(var j = 2; j <= n; j++)
+				  {
+					  if(i%j === 0 && j != i)
+					  {
+						  check = false;
+					  }
+				  }
+
+				  if(check === true )
+				  { 
+				  	//if i  equal to 1 skip 
+				  	if(i===1){
+				  		continue;
+				  	}else{
+				  		//if i is prime add it to primeNum
+					  primeNum.push(i);
+				  	}
+				    
+				  }
+				
+				  check = true;
+			}
+
+			return primeNum;
+		}
+	}
+};
+    
